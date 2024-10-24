9d2a5d4053797ed77a8d6ba5bacc1864579870ed
Ygor Gama <ygor.bio.2002@gmail.com>
2024-10-23 22:26:48 -0300

    features: Authentication and security completed

---
+++ src/database/seeders/20241024000153-demo-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { hashSync } = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   return queryInterface.bulkInsert('users', [
+      {
+        name: 'John',
+        email: 'example@example.com',
+        password: hashSync("example", 12),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },  
+   ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
